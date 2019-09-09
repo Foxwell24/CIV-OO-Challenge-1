e38768dc3d9ef8593cbd932193ddf0bdd7a25189
Kael Foxwell <kaefoxewe@gmail.com>
2019-09-09 16:43:53 +1000

    nearly finished the challenge

---
+++ ClassLibrary/Shapes.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+
+
+namespace ClassLibrary
+{
+    public abstract class Shape
+    {
+        public string color;
+
+        public Shape(string color)
+        {
+            this.color = color;
+        }
+    }
+    public abstract class Quadralaterals : Shape
+    {
+        public int side1Length;
+        public int side2Length;
+        public int side3Length;
+        public int side4Length;
+
+        public Quadralaterals(string color, int side1Length, int side2Length, int side3Length, int side4Length) : base(color)
+        {
+            this.side1Length = side1Length;
+            this.side2Length = side2Length;
+            this.side3Length = side3Length;
+            this.side4Length = side4Length;
+        }
+
+        public Quadralaterals(int side1Length, string color) : base(color)
+        {
+            this.side1Length = side1Length;
+        }
+
+        public Quadralaterals(int side1Length, int side2Length, string color) : base (color)
+        {
+            this.side1Length = side1Length;
+            this.side2Length = side2Length;
+        }
+        public void SetLengths(int s1, int s2)
+        {
+            side1Length = s1;
+            side2Length = s2;
+        }
+    }
+
+    public class Rectangle : Quadralaterals
+    {
+        public Rectangle(string color, int side1Length, int side2Length) : base(side1Length, side2Length, color)
+        {
+            
+        }
+
+        public int GetArea()
+        {
+            return side1Length * side2Length;
+        }
+        public int GetPerimeter()
+        {
+            return side1Length * 2 + side2Length * 2;
+        }
+    }
+
+    public class Square : Quadralaterals
+    {
+        public Square(string color, int side1Length) : base(side1Length, color)
+        {
+            
+        }
+        public int GetArea()
+        {
+            return side1Length * side1Length;
+        }
+        public int GetPerimeter()
+        {
+            return side1Length * 4;
+        }
+    }
+}
---
+++ UserInterface/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using ClassLibrary;
+
+namespace UserInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (0 == 0)
+            {
+                int s1 = 0;
+                int s2 = 0;
+
+                Console.WriteLine("What color is your shape?");
+                string color = Console.ReadLine();
+
+                Console.WriteLine("Is it a [R]ectange or [S]quare ?");
+                ConsoleKeyInfo input = Console.ReadKey();
+                Console.WriteLine();
+                if (input.Key.ToString().ToLower() == "r")
+                    MakeRectangle(color, 0);
+                if (input.Key.ToString().ToLower() == "s")
+                    MakeSquare(color);
+                        
+            }
+        }
+
+        private static Square MakeSquare(string color)
+        {
+            int s1 = 0;
+            Console.WriteLine("length of side 1?");
+            try
+            {
+                s1 = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("not an integer");
+                MakeSquare(color);
+            }
+            Square i = new Square(color, s1);            
+            return i;
+        }
+
+        public static Rectangle MakeRectangle(string color, int s1)
+        {
+            int s2 = 0;
+            if(s1 == 0)
+            {
+                Console.WriteLine("length of side 1?");
+                try
+                {
+                    s1 = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("not an integer");
+                    MakeRectangle(color, 0);
+                }
+            }
+            Console.WriteLine("length of side 2?");
+            try
+            {
+                s2 = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("not an integer");
+                MakeRectangle(color, s1);
+            }
+            Console.Clear();
+
+            Rectangle i = new Rectangle(color, s1, s2);
+            return i;
+        }
+    }
+}
+
